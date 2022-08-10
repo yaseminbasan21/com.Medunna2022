e4b82202484c7cc61dc7b3b1e8e85e627ae1547d
huseyinercik <huseyincanercik@gmail.com>
2022-08-10 22:11:48 +0300

    class lar eklendi "09-08-2022"

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml"}, // bu satir ile html,xml,json report olusturabiliyoruz
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@DBtest",
+        tags = "",
         dryRun = false // step definition olusturmak icin calistirdiginda burayı true yapmayi unutma
 )
 
