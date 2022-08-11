b157905db7328a56ddecc9282344dbf36c5e1d8f
Bahadır Koramaz <bkork1maz@gmail.com>
2022-08-12 00:06:16 +0300

    12.08.22

---
+++ src/test/java/stepdefinitions/US_009_stepdefinitions.java
@@ -0,0 +1,127 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class US_009_stepdefinitions {
+
+    @Given("TG kullanici {string} sayfasina gider")
+    public void tg_kullanici_sayfasina_gider(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar")
+    public void tg_sayfanin_sağ_üst_köşesinde_yeralan_insan_ikonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG acilan dropdown menude Sign in butonuna tıklar")
+    public void tg_acilan_dropdown_menude_sign_in_butonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG username olarak {string} girer")
+    public void tg_username_olarak_girer(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG password olarak {string} girer")
+    public void tg_password_olarak_girer(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Sign in butonuna basar")
+    public void tg_sign_in_butonuna_basar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG MYPAGES dropdown menusune tıklar")
+    public void tg_mypages_dropdown_menusune_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Search Patient butonuna tıklar")
+    public void tg_search_patient_butonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Patients tablosunun ve hasta bilgilerinin gorunur oldugunu test eder")
+    public void tg_patients_tablosunun_ve_hasta_bilgilerinin_gorunur_oldugunu_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG sayfayi kapatir")
+    public void tg_sayfayi_kapatir() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("TG Tabloda bilgilerini düzenlenmek istedigi hastanın yanındaki edit butonuna tıklar")
+    public void tg_tabloda_bilgilerini_düzenlenmek_istedigi_hastanın_yanındaki_edit_butonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Hasta bilgilerinin düzenlenebilir oldugunu test eder")
+    public void tg_hasta_bilgilerinin_düzenlenebilir_oldugunu_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Patient SSN kısmına hastanın SSN bilgisini girer")
+    public void tg_patient_ssn_kısmına_hastanın_ssn_bilgisini_girer() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Ilgili hastanın yanındaki view butonuna tıklar")
+    public void tg_ılgili_hastanın_yanındaki_view_butonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG kayit bilgilerinin gorunur oldugunu test eder")
+    public void tg_kayit_bilgilerinin_gorunur_oldugunu_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG {int} saniye bekler")
+    public void tg_saniye_bekler(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG kayit bilgilerini siler ve Save butonuna tıklar")
+    public void tg_kayit_bilgilerini_siler_ve_save_butonuna_tıklar() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG bilgilerin silinip silinmedigini test eder")
+    public void tg_bilgilerin_silinip_silinmedigini_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("TG Hasta idsini silemedigini test eder")
+    public void tg_hasta_idsini_silemedigini_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("TG Arama yapabildigini ve istenilen hastanın gorunur oldugunu test eder")
+    public void tg_arama_yapabildigini_ve_istenilen_hastanın_gorunur_oldugunu_test_eder() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ target/test-classes/feature/US_009.feature
@@ -0,0 +1,83 @@
+Feature: US009 Staff (Personel), hasta bilgilerini görebilmeli, düzenleyebilmeli ve silebilmelidir
+
+
+  Scenario: TC01_Staff My Pages sekmesinden hasta bilgilerini görebilmelidir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    Then TG Patients tablosunun ve hasta bilgilerinin gorunur oldugunu test eder
+    And TG sayfayi kapatir
+
+
+  Scenario: TC02_Staff bütün hasta bilgilerini düzenleyebilmelidir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    Then TG Tabloda bilgilerini düzenlenmek istedigi hastanın yanındaki edit butonuna tıklar
+    And TG Hasta bilgilerinin düzenlenebilir oldugunu test eder
+    And TG sayfayi kapatir
+
+  Scenario: TC03_Staff, SSN'lerine göre yeni bir başvuru sahibi arayabilmeli ve tüm kayıt bilgilerinin doldurulduğunu görebilmelidir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    And TG Patient SSN kısmına hastanın SSN bilgisini girer
+    Then TG Ilgili hastanın yanındaki view butonuna tıklar
+    And TG kayit bilgilerinin gorunur oldugunu test eder
+    And TG sayfayi kapatir
+
+  Scenario: TC06_Staff herhangi bir hasta bilgisini silebilir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    Then TG Tabloda bilgilerini düzenlenmek istedigi hastanın yanındaki edit butonuna tıklar
+    And TG 1 saniye bekler
+    And TG kayit bilgilerini siler ve Save butonuna tıklar
+    And TG bilgilerin silinip silinmedigini test eder
+    And TG sayfayi kapatir
+
+  Scenario: TC07_Staff hastaları silememelidir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    Then TG Tabloda bilgilerini düzenlenmek istedigi hastanın yanındaki edit butonuna tıklar
+    And TG Hasta idsini silemedigini test eder
+    And TG sayfayi kapatir
+
+  Scenario: TC06_Staff hastaları SSN kimliklerine göre arayabilmelidir.
+    Given TG kullanici "MedunnaUrl" sayfasina gider
+    And TG sayfanin sağ üst köşesinde yeralan insan ikonuna tıklar
+    Then TG acilan dropdown menude Sign in butonuna tıklar
+    And TG username olarak "StaffUsernameTG" girer
+    And TG password olarak "StaffPasswordTG" girer
+    And TG Sign in butonuna basar
+    Then TG MYPAGES dropdown menusune tıklar
+    And TG Search Patient butonuna tıklar
+    And TG Patient SSN kısmına hastanın SSN bilgisini girer
+    And TG Arama yapabildigini ve istenilen hastanın gorunur oldugunu test eder
+    And TG sayfayi kapatir
